1b16b7a85d37e536aa78dc201e22577906ec09bb
litao <15105695443@163.com>
2018-05-14 09:46:05 +0800

    尝试上传到githup上

---
+++ demo-test/src/chs/litao/demo/demo01.java
@@ -0,0 +1,13 @@
+package chs.litao.demo;
+
+public class demo01 {
+
+	
+	
+	
+	public static void main(String[] args) {                                      
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+	}
+
+}
---
+++ demo-test/src/chs/litao/demo/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Administrator
+ *
+ */
+package chs.litao.demo;
